4d6310bb1e4a5ca14ca1146800cf175a432c1a0a
Enisco29 <opeyemioluwafisayo29@email.com>
2025-08-27 00:43:58 +0100

    first-commit

---
+++ py-class-1.py
@@ -0,0 +1,68 @@
+# name = "eniola" #string
+# age = 17 #Interger
+# score = 80.5 #float
+# is_student = True #Boolean
+
+# print(type(name))
+# print(type(age))
+# print(type(score))
+# print(type(is_student))
+
+# name = input("Enter your Name: ")
+# age = input("Enter your Age: ")
+# score = input("Enter your Score: ")
+# is_student= input("Are you a student? (True/False); ")
+
+# print()
+# print("Name:", name)   
+# print("Age:", age)
+# print("Score:", score)
+# print("Is a student:", is_student)
+
+#Arithmetics operators
+#Addition: +
+#Subraction: -
+#Division: /
+#Interger_Division: //
+#multiplication: *
+#exponent: **
+#modulus_division: %
+
+#Type Conversion\
+
+int_num = 1
+str_num = str(int_num)
+bool_num = bool(int_num)
+float_num = float(int_num)
+
+print(type(int_num))
+print(type(str_num))
+print(type(bool_num))
+print(type(float_num))
+
+print(int_num)
+print(str_num)
+print(bool_num)
+print(float_num)
+
+str_name = "Eniola"
+bool_name = bool(str_name)
+
+print(type(str_name))
+print(type(bool_name))
+
+print(str_name)
+print(bool_name)
+
+
+
+# num1 = input("Enter first num; ")
+# num2 = input("Enter second number; ")
+
+# print("Addition:", num1 + num2 )
+# print("Subraction:", num1 - num2 )
+# print("Division:", num1 / num2 )
+# print("Interger Division:", num1 // num2 )
+# print("Multiplication:", num1 * num2 )
+# print("Exponent:", num1 ** num2 )
+# print("Modulus Division:", num1 & num2 )
