1c4da7c3395f1e184de9e6c3856e3e7ba0c5a370
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-11-25 22:46:28 +0100

    learn data_types, typecasting. e.t.c

---
+++ main.py
@@ -0,0 +1,32 @@
+#This is my first python program
+# print("I love plantain")
+# print("It's really tasty!")
+
+#variables
+
+first_name = "Eniola"
+food = "plaintain"
+email = "fake.gmail.com"
+
+age=3 
+
+is_student = True
+
+if is_student: 
+    print("You are a student")
+else: 
+    print("You are not a student")
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+
+
+
+
+
+is_rich = False
+
+if is_rich: 
+    print('flex your money')
+else: print("keep coding")
---
+++ type_casting.py
@@ -0,0 +1,20 @@
+#str(), int(), float(), bool()
+
+# name = "Opeyemi Eniola"
+# age = 17
+# gpa = 5.0
+# is_student = True
+
+# gpa = int(gpa)
+# age = float(age)
+# age = str(age)
+
+# print(type(age))
+
+name = input("What is yor name?:")
+age = int(input("What is ur age?: "))
+
+age = age + 1
+
+print(f"Hello {name}")
+print(f"you are now {age} years old")
