fce7b8f46a9e9de0100b8ffed06ea16050749cec
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-11-26 15:28:33 +0100

    if statement

---
+++ exercise.py
@@ -0,0 +1,26 @@
+# lenght = int(input("enter the length"))
+# width = int(input("enter the width"))
+# area = lenght * width
+
+# print(f"the area is {area}")
+
+# adjective1 = input("Enter an adjective: (description)")
+# noun1 = input("Enter a noun: (person, place, or thing)")
+# adjective2 = input("Enter another adjective: (description)")
+# verb1 = input("Enter a verb: (doing word)")
+# adjective3 = input("Enter one more adjective: (description)") 
+
+# print(f"Today I went to a {adjective1} zoo.")
+# print(f"In an exhibit, I saw a {noun1}")
+# print(f"{noun1} was {adjective2} and {verb1}")
+# print(f"I was {adjective3}")
+
+x = 3.14
+y = 4
+z = 5
+
+# result = round(x)
+# result = abs(y)
+result = pow(2, 3)
+
+print(result)
---
+++ inbuilt.py
@@ -0,0 +1,32 @@
+import math
+
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(9)
+# print(result)
+
+# radius = float(input(f"What is your raius?: "))
+# circumference = int(2 * math.pi * radius)
+
+# print(f"circumference is {circumference}")
+
+# radius = int(input("Enter the radius"))
+# area = math.pi * pow(radius, 2)
+
+# print(area)
+
+# a = float(input("Input a"))
+# b = float(input("Input b"))
+
+# c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+# print(c)
+
+age = int(input("What is your age: "))
+
+if age >= 18: 
+    print("You can enter")
+elif age < 0:
+    print("Stop fucking lying")
+else:
+    print("Fuck off")
