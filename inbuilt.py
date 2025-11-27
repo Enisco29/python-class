3ed51a4bb1dce592cd33a4113f88e8008d7f7ae3
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-11-27 13:38:30 +0100

    practice with timer

--- inbuilt.py
+++ inbuilt.py
@@ -61,4 +61,76 @@ import math
 #     print("Username should be aplabet")
 # else:
 #      print(f"Your username is {user_input}")
-    
+
+# number = "1234-5678-9012-3456"
+
+# print(number[0:4])
+
+# price1 = 3.14159
+# price2 = -987.65
+# price3 = 12.34
+
+# print(f"price 1 is {price1:+}")
+# print(f"price 1 is {price2:+}")
+# print(f"price 1 is {price3}")
+
+# name = input("Enter your name: ")
+
+# while name == "": 
+#      print("You did not enter ur name")
+#      name = input("Enter your name: ") 
+# print(f"Hello {name}")
+
+# food = input("what food do you like: ")
+
+# while not food == 'q':
+#     print(f"You like {food}")
+#     food = input("what another food do you like: ")
+
+# print("bye")
+
+# age = int(input("What is your age: "))
+
+# while age < 0:
+#     print("type ur correct age")
+#     age = int(input("What is your age: "))
+# print(f"your age is {age}")
+
+# principle = 0
+# rate = 0
+# time = 0
+
+# while principle <= 0:
+#     principle = float(input("Enter the principle amount: "))
+#     if principle <= 0:
+#         print("Principle can't be less than or equal to zero")
+
+# while rate <= 0:
+#     rate = float(input("Enter the rate amount: "))
+
+#     if rate <= 0:
+#        print("rate can't be less than or equal to zero")
+
+# while time <= 0:
+#     time = float(input("Enter the time amount: "))
+#     if time <= 0:
+#         print("time can't be less than or equal to zero")
+
+
+# total = principle * pow((1 + rate/100), time)
+# print(f"Balance after {time} years: ${total:.2f}")
+
+# food = ["lol", "lol", "lol"]
+
+# for x in (food):
+#     print(x)
+
+# no = "1234556677889"
+
+# for x in range(1,20):
+#     if x == 4:
+#         break
+#     else:
+#         print(x)
+
+
