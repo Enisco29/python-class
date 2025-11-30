6cb9bd3d87085753eaf1c32b314ac61a49652bfd
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-11-30 23:10:11 +0100

    keyword arguments

--- functions.py
+++ functions.py
@@ -13,11 +13,20 @@
 #     z = x = y
 #     return z
 
-def create_name(first, last):
-    first = first.upper()
-    last = last.capitalize() 
-    return first + " " + last
+# def create_name(first, last):
+#     first = first.upper()
+#     last = last.capitalize() 
+#     return first + " " + last
 
-full_name = create_name("Ope", "Eniola")
+# full_name = create_name("Ope", "Eniola")
 
-print(full_name)
+# print(full_name)
+
+# print("1", "2", "3", sep="-")
+
+def get_phone(country, area, first, last):
+    return f"{country}-{area}-{first}-{last}"
+
+phone_num = get_phone(country=234, area=704, first=4357, last=1913)
+
+print(phone_num)
