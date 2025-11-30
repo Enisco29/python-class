2361df6a06439081c4ea74d3244d709ae7ba1750
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-11-30 23:47:16 +0100

    done for today

--- type_casting.py
+++ type_casting.py
@@ -11,10 +11,36 @@
 
 # print(type(age))
 
-name = input("What is yor name?:")
-age = int(input("What is ur age?: "))
+# name = input("What is yor name?:")
+# age = int(input("What is ur age?: "))
 
-age = age + 1
+# age = age + 1
 
-print(f"Hello {name}")
-print(f"you are now {age} years old")
+# print(f"Hello {name}")
+# print(f"you are now {age} years old")
+
+# def add(*args):
+#     total = 0
+#     for arg in args:
+#         total += arg
+#     return total
+    
+# print(add(1, 2, 3))
+
+# numbers = [1, 2,3 ,4 ,5 ]
+
+# for number in numbers:
+#     print(number)
+
+email = "ope@gmailcom"
+
+if "@" in email or "." in email:
+    print("lol")
+
+doubles = [x*2 for x in range(1, 11)]
+print(doubles)
+
+def is_week(dat):
+    match(dat):
+        case "lol":
+            return True
