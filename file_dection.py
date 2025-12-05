86766672ef5c98f829375504c5c0d82174d2f53b
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-12-05 23:31:58 +0100

    might tske a break

---
+++ file_dection.py
@@ -0,0 +1,35 @@
+# Python file detection
+
+# import os
+
+# file_path = "file.txt"
+
+# if os.path.exists(file_path):
+#     print(f"The location {file_path} exists")
+# else:
+#     print(f"The location {file_path} does not exist")
+
+#python writing files(.txt, .csv, .json)
+
+# txt_data = "I like pizza!"
+
+# file_path = "output.txt"
+
+# with open(file_path, "w") as file:
+#     file.write(txt_data)
+#     print(f"txt file '{file_path}' created successfully")
+
+# with open(file_path, "r") as file:
+#     content = file.read()
+#     print("File content:")
+#     print(content)
+
+# import datetime
+
+# date = datetime.date(2024, 6, 15)
+# today = datetime.date.today()
+# now = datetime.datetime.now()
+
+# now = now.strftime("%H:%M:%S %m/%d/%Y")
+
+# print(now)
