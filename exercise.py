0f94c81ef61458b43c675f697413a03e4b714d8c
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-12-11 12:28:58 +0100

    add alarm

--- exercise.py
+++ exercise.py
@@ -25,16 +25,48 @@
 
 # print(result)
 
-capitals = {
-            "USA": "LA",
-            "India": "New Dehli",
-            "China": "Beijing"
-            }
+# capitals = {
+#             "USA": "LA",
+#             "India": "New Dehli",
+#             "China": "Beijing"
+#             }
 
 # print(capitals.get("US"))
 # capitals.update({"germany": "Berlin"})
 
-items = capitals.items()
+# items = capitals.items()
 
-for key, vlaue in items:
-    print(f"{key}: {vlaue}")
+# for key, vlaue in items:
+#     print(f"{key}: {vlaue}")
+    
+    
+import threading
+import time
+
+def walk_dog(first, last):
+    time.sleep(8)
+    print(f"You finish walking {first} {last}")
+    
+def take_out_trash():
+    time.sleep(2)
+    print("You take out trash")
+    
+def get_mail():
+    time.sleep(4)
+    print("You get the mail")
+    
+    
+chore1 = threading.Thread(target=walk_dog, args=("Scooby", "doo"))
+chore1.start()
+
+chare2 = threading.Thread(target=take_out_trash)
+chare2.start()
+
+chare3 = threading.Thread(target=get_mail)
+chare3.start()
+
+chore1.join()
+chare2.join()
+chare3.join()
+
+print("All chores are complete")
